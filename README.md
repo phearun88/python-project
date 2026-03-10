e089e5edcd9caf25b8e1169e53c50e21a568a35f
phearun168 <phin.phearun2013@gmail.com>
2026-03-09 19:21:13 -0500

    enabling agent history

--- README.md
+++ README.md
@@ -6,13 +6,20 @@ A collection of **20+ Python projects** I’m building to sharpen my skills in:
 - 
 - (Soon) AI + Agents projects
 
-Here are some of the Python projects currently under development:
+## Featured Python Projects currently under development:
 
-1. **Build a Task Manager App** : A Streamlit-based web application for managing tasks efficiently. 
-   1. **Run** _Navigate to the task-management directory and execute streamlit run web.py_
-2. **PDF Generator** : _A Python application for creating and exporting PDF documents programmatically._
-3. **Build an Email News App** : _this app access news about a particular topic and sends them by email_.
-4. **AI-Enhanced Task Management Application** : _An improved version of the Task Management App that integrates AI agent capabilities for smarter task handling and automation._
+1. 🗂️ **Task Management Web Application**  
+   A web-based task management system built with Streamlit for tracking and organizing daily activities.
+<br/><br/>
+2. 🤖 **AI-Powered Task Management Application**  
+   A next-generation task manager enhanced with AI agent functionality to support intelligent planning, automation, and productivity.
+<br/><br/>
+3. 📑 **PDF Generation Utility**  
+   A document automation tool designed to generate PDF files programmatically using Python.
+<br/><br/>
+4. 📧 **Email News Delivery Application**  
+   A news automation system that collects topic-specific news and distributes updates through email.
+
 
 > More projects will be added as development progresses.
 
--- task_manager_ai_agent/ai_agent.py
+++ task_manager_ai_agent/ai_agent.py
@@ -10,6 +10,7 @@ from pydantic_core.core_schema import model_field
 
 from langchain.tools import tool
 from langchain.agents import create_openai_tools_agent, AgentExecutor
+from todoist_api_python.api import TodoistAPI
 
 #Loads environment variables from a .env
 load_dotenv()
@@ -17,11 +18,16 @@ load_dotenv()
 todoist_api_key = os.getenv("TODOIST_API_KEY")
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
+todoist = TodoistAPI(todoist_api_key)
+
 @tool
-def add_task():
+def add_task(task, desc= None):
     """Add a new task to the user's tasks list. user this when the user wants to add a new task"""
 
-    print("Adding task")
+    todoist.add_task(content=task,
+                     description=desc)
+
+    print(task)
     print("Task added")
 
 tools = [add_task ]
@@ -34,12 +40,15 @@ llm = ChatGoogleGenerativeAI(
 )
 
 
-system_prompt ="You are a helpful assistant. You will help me to find girl friend"
-user_input= "Can you tell me the task to get girlfriend?"
+system_prompt ="You are a helpful assistant. You will help the user add tasks"
+
+
 prompt = ChatPromptTemplate([
     ("system", system_prompt),
-    ("user", user_input),
-    MessagesPlaceholder("agent_scratchpad")
+    MessagesPlaceholder("history"),
+    ("user", "{input}"),
+    MessagesPlaceholder("agent_scratchpad"),
+
 ])
 
 #chain = prompt | llm | StrOutputParser()
@@ -51,5 +60,12 @@ agent_executor = AgentExecutor(agent = agent, tools = tools, verbose=True)
 
 
 #response = chain.invoke({"input": user_input})
-response = agent_executor.invoke({"input": user_input})
-print(response)
+
+
+history = []
+while True:
+    user_input = input("You: ")
+    response = agent_executor.invoke({"input": user_input, "history": history})
+    print(response['output'])
+    history.append(HumanMessage(content=user_input))
+    history.append(AIMessage(content=response['output']))
