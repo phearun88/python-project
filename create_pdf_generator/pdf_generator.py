4d0467b12c1721ab6dcb22e0ae42d6b9327d501a
phearun168 <phin.phearun2013@gmail.com>
2026-03-15 15:47:25 -0400

    fix bug

--- create_pdf_generator/pdf_generator.py
+++ create_pdf_generator/pdf_generator.py
@@ -29,13 +29,11 @@ for index, row in df.iterrows():
         pdf.add_page()
         # Set the footer
         pdf.ln(265)
-
         for y in range(20, 298, 10):
             pdf.line(10, y, 200, y)
 
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
-
 
 pdf.output("output.pdf")
