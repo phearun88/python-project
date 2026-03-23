d96707c1689bc49ae87bf55228956f3a9f8ddd92
phearun168 <phin.phearun2013@gmail.com>
2026-03-22 21:23:54 -0500

    test

--- task-management/web.py
+++ task-management/web.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import function
 
-
 todos = function.get_todos()
 
 def add_todo():
@@ -9,11 +8,9 @@ def add_todo():
     todos.append(todo)
     function.write_todos(todos)
 
-
 st.title('My Todo App')
 st.subheader('This is your todo application.')
 st.write("This app to increase your productivity.")
-
 
 for index, todo in enumerate(todos):
    checkbok = st.checkbox(todo, key=todo)
@@ -22,8 +19,6 @@ for index, todo in enumerate(todos):
        function.write_todos(todos)
        del st.session_state[todo]
        st.rerun()
-
-
 
 st.text_input(label="", placeholder="Enter your todo...", on_change=add_todo, key='new_todo')
 #st.session_state
